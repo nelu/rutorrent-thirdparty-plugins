960e19161115b8ec7aacce01c560b747c9f3c7b4
hwk <hwk@a528ebbd-8654-e011-9671-00248cbe6ec3>
2011-03-27 09:49:48 +0000

    Working version
    git-svn-id: http://svn.rutorrent.org/svn/filemanager/trunk@14 a528ebbd-8654-e011-9671-00248cbe6ec3

---
+++ fileshare/conf.php
@@ -0,0 +1,9 @@
+<?php
+
+
+// path on domain where a symlink to share.php can be found
+// example: http://mydomain.com/share.php
+$downloadpath = 'http://robits.org/rutorrent/share.php'; 
+
+
+?>
---
+++ fileshare/settings.js.php
@@ -0,0 +1,6 @@
+<?php
+include('conf.php');
+
+echo 'theWebUI.FS.downlink = "',$downloadpath, "\";\n";
+
+?>
