3509d7e72dc48dee3d59328029043ab975179e3a
Abi <abixalmon@gmail.com>
2018-07-16 01:16:09 +0600

    Play videos using HTML5 video player

--- mediastream/conf.php
+++ mediastream/conf.php
@@ -3,7 +3,7 @@
 // path on domain where a symlink to view.php can be found
 // change only if you use web AUTH
 // example: http://mydomain.com/stream/view.php
-$streampath = 'http://mydomain.com/stream/view.php'; 
+$streampath = 'plugins/mediastream/view.php';
 
 
 ?>
--- mediastream/view.php
+++ mediastream/view.php
@@ -1,46 +1,142 @@
 <?php
 
-
-if(!isset($_GET['ses'])) {die('404 bUfU');}
+if (!isset($_GET['ses'])) {
+    die('404 bUfU');
+}
 $oldses = session_id();
-if(!empty($oldses)) {die('404 Its not for you');}
+if (!empty($oldses)) {
+    die('404 Its not for you');
+}
 
 session_id($_GET['ses']);
 session_start();
 
 $_SERVER['REMOTE_USER'] = $_SESSION['uname'];
 
+unset($_POST);
+$_POST = $_GET;
 
-unset($_POST); $_POST = $_GET;
-
-
-require_once( dirname(__FILE__)."/../../php/xmlrpc.php" );
-require_once( dirname(__FILE__)."/../filemanager/flm.class.php" );
-require_once( dirname(__FILE__)."/../filemanager/xmlfix.php" );
-
+require_once(dirname(__FILE__) . "/../../php/xmlrpc.php");
+require_once(dirname(__FILE__) . "/../filemanager/flm.class.php");
+require_once(dirname(__FILE__) . "/../filemanager/xmlfix.php");
 
 eval(getPluginConf('filemanager'));
 
-class vs extends FLM {
+class vs extends FLM
+{
+    private $path = "";
+    private $stream = "";
+    private $buffer = 102400;
+    private $start = -1;
+    private $end = -1;
+    private $size = 0;
 
-	public function stream($file) {
+    public function view($file)
+    {
+        $this->shout = false;
 
-		$this->shout = FALSE;
+        if (!preg_match('/^(avi|divx|mpeg|mp4|mkv)$/i', $this->fext($file))) {
+            $this->sdie('404 Invalid format');
+        }
 
-		if (!preg_match('/^(avi|divx|mpeg|mp4|mkv)$/i', $this->fext($file))) {$this->sdie('404 Invalid format');}
+        if (!is_file($this->workdir . $file)) {
+            $this->sdie('404 File not found');
+        }
 
-		if (!is_file($this->workdir.$file)) {$this->sdie('404 File not found');}
+        $this->path = $this->workdir . $file;
 
-		header('Content-Type: video/divx');
-		header('Content-Disposition: inline; filename="'.$file.'"');
+        $this->start();
+    }
 
-		$this->get_file($this->workdir.$file);
-	}
+    function start()
+    {
+        $this->open();
+        $this->setHeader();
+        $this->stream();
+        $this->end();
+    }
+
+    private function open()
+    {
+        if (!($this->stream = fopen($this->path, 'rb'))) {
+            $this->sdie('Could not open stream for reading');
+        }
+    }
+
+    private function setHeader()
+    {
+        ob_get_clean();
+        header("Content-Type: video/mp4");
+        header("Cache-Control: max-age=2592000, public");
+        header("Expires: " . gmdate('D, d M Y H:i:s', time() + 2592000) . ' GMT');
+        header("Last-Modified: " . gmdate('D, d M Y H:i:s', @filemtime($this->path)) . ' GMT');
+        $this->start = 0;
+        $this->size = filesize($this->path);
+        $this->end = $this->size - 1;
+        header("Accept-Ranges: 0-" . $this->end);
+
+        if (isset($_SERVER['HTTP_RANGE'])) {
+
+            $c_start = $this->start;
+            $c_end = $this->end;
+
+            list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+            if (strpos($range, ',') !== false) {
+                header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                header("Content-Range: bytes $this->start-$this->end/$this->size");
+                exit;
+            }
+            if ($range == '-') {
+                $c_start = $this->size - substr($range, 1);
+            } else {
+                $range = explode('-', $range);
+                $c_start = $range[0];
+
+                $c_end = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $c_end;
+            }
+            $c_end = ($c_end > $this->end) ? $this->end : $c_end;
+            if ($c_start > $c_end || $c_start > $this->size - 1 || $c_end >= $this->size) {
+                header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                header("Content-Range: bytes $this->start-$this->end/$this->size");
+                exit;
+            }
+            $this->start = $c_start;
+            $this->end = $c_end;
+            $length = $this->end - $this->start + 1;
+            fseek($this->stream, $this->start);
+            header('HTTP/1.1 206 Partial Content');
+            header("Content-Length: " . $length);
+            header("Content-Range: bytes $this->start-$this->end/" . $this->size);
+        } else {
+            header("Content-Length: " . $this->size);
+        }
+
+    }
+
+    private function end()
+    {
+        fclose($this->stream);
+        exit;
+    }
+
+    private function stream()
+    {
+        $i = $this->start;
+        set_time_limit(0);
+        while (!feof($this->stream) && $i <= $this->end) {
+            $bytesToRead = $this->buffer;
+            if (($i + $bytesToRead) > $this->end) {
+                $bytesToRead = $this->end - $i + 1;
+            }
+            $data = fread($this->stream, $bytesToRead);
+            echo $data;
+            flush();
+            $i += $bytesToRead;
+        }
+    }
 }
 
-
-
 $s = new vs();
-$s->stream($s->postlist['target']);
+$s->view($s->postlist['target']);
 
 ?>
